8ce69feeb14acf80efcdfd046066763ac5efb9de
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-03-02 00:49:15 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.c
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -3,9 +3,6 @@ int main()
     int x;
     int y;
     scanf("%d %d",&x, &y);
-    printf("%d\n", x+y);
-    printf("%d\n", x-y);
-    printf("%d\n", x*y);
-    printf("%d\n", x/y);
-    printf("%d\n", x%y);    
+    printf("%d\n%d\n%d\n%d\n%d\n", x+y, x-y, x*y, x/y, x%y);
+    
 }
