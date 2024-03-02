b488286ab4e0b93c09f67edbbcb321e770067b77
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-03-02 11:47:12 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x%4==0 && x%100!=0)
+        printf("1");
+    else if(x%4==0 && x%400==0)
+        printf("1");
+    else
+        printf("0");
+    return 0;
+}
