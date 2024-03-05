5e22c67067a31b03ba9a5b5ad9ffe5ffb656c73e
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-03-05 10:18:24 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int x,y,i,t;
+    scanf("%d",&t);
+    for(i=1;i<=t;i++)
+    {
+        for(x=1;x<=i;x++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
