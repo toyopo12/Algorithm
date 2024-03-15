8d283dd96a3b2ff6ddc3978fcb43a776d49af497
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-03-15 20:59:13 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.c
@@ -0,0 +1,18 @@
+int arr[42];
+
+int main(){
+	int x;
+	int ans = 0;
+	
+	for(int i=0;i<10;i++){
+		scanf("%d",&x);
+		arr[x%42] = 1;
+	}
+	
+	for(int i=0;i<42;i++)
+		ans += arr[i];
+	
+	printf("%d",ans);
+	
+	return 0;
+}
