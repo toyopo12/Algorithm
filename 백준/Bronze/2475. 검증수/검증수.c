0f968a7fc950922756df4481b419732630acba63
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-03-25 13:36:37 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int a,b,c,d,e;
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+    printf("%d",(a*a+b*b+c*c+d*d+e*e)%10);
+    return 0;
+}
