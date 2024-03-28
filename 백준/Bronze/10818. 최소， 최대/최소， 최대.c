997e3eeacb8c880df16fad6afa91460b516529cd
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-03-28 18:48:23 +0900

    [Bronze III] Title: 최소, 최대, Time: 176 ms, Memory: 4892 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.c
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -1,19 +1,22 @@
-int main(void){
+int main()
+{
     int N;
     scanf("%d", &N);
     int arr[N];
-    
-    for(int i=0; i<N; i++){
+    for(int i=0; i<N; i++)
+    {
         scanf("%d", &arr[i]);
     }
  
-    int min = arr[0], max = arr[0];
-    for(int j=0; j<N; j++){
+    int min = arr[0];
+    int max = arr[0];
+    for(int j=0; j<N; j++)
+    {
         if(arr[j] < min)
             min = arr[j];
         if(arr[j] > max)
             max = arr[j];
     }
- 
-    printf("%d %d", min, max);
+    printf("%d %d",min,max);
+    return 0;
 }
