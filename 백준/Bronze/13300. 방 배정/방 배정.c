fc6a46b78ba2914d7004e3ed0e50ce1ba14ab81f
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-04-08 14:41:53 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 1112 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.c
@@ -0,0 +1,31 @@
+int main()
+{
+    int N,K;
+    int W[7]={0,};
+    int M[7]={0,};
+    int sum=0;
+    scanf("%d %d",&N,&K);
+    for(int a=0;a<N;a++)
+    {
+        int S,Y;
+        scanf("%d %d",&S,&Y);
+        if(S==0)
+        {
+            W[Y]++;
+        }
+        else
+        {
+            M[Y]++;
+        }
+    }
+    for(int a=1;a<=6;a++)
+    {
+        sum+=W[a]/K+1+M[a]/K+1;
+        if(W[a]%2==0)
+            sum--;
+        if(M[a]%2==0)
+            sum--;
+    }
+    printf("%d",sum);
+    return 0;
+}
