026b4dc417d04cef44e2cc355ec3d3125a5a775d
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-04-11 21:14:18 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 184 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.c
@@ -0,0 +1,35 @@
+void hi(int x, int y, int n) 
+{
+	if((x/n)%3==1 && (y/n)%3==1)
+    {
+		printf(" ");
+	}
+	else 
+    {
+		if(n/3==0) 
+        {
+			printf("*");
+		}
+		else 
+        {
+			hi(x,y,n/3);
+		}
+	}
+}
+
+int main() 
+{
+	int n;
+	scanf("%d", &n);
+
+	for (int i=0;i<n;i++) 
+    {
+		for (int j=0;j<n;j++) 
+        {
+			hi(i,j,n);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
