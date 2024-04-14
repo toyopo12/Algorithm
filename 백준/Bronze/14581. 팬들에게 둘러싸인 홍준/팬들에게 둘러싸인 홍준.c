668d3b5bafc23e462d9d2c2fed372f56175c696a
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-04-14 23:04:19 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.c
@@ -0,0 +1,17 @@
+int main()
+{
+    char s[21];
+    scanf("%s",s);
+    for(int a=0;a<3;a++)
+    {
+        printf(":fan:");
+    }
+    printf("\n:fan:");
+    printf(":%s:",s);
+    printf(":fan:\n");
+    for(int a=0;a<3;a++)
+    {
+        printf(":fan:");
+    }
+    return 0;
+}
