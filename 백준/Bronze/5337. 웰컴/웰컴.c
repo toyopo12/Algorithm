b806ce023503002615d63998edf4f67d8aca598a
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-04-22 00:29:02 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    printf(".  .   .\n"
+           "|  | _ | _. _ ._ _  _\n"
+           "|/\\|(/.|(_.(_)[ | )(/.");
+}
