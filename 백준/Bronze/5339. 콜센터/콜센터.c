154e26755e8d0ca7c62ba6001db3ec50a145b828
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-05-12 15:23:57 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    printf("     /~\\\n"
+           "    ( oo|\n"
+           "    _\\=/_\n"
+           "   /  _  \\\n"
+           "  //|/.\\|\\\\\n"
+           " ||  \\ /  ||\n"
+           "============\n"
+           "|          |\n"
+           "|          |\n"
+           "|          |");
+    return 0;
+}
