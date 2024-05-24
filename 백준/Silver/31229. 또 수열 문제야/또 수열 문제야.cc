97bb18d11b1708169f796420b569ce3e32934ead
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-05-24 20:53:14 +0900

    [Silver V] Title: 또 수열 문제야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31229. 또 수열 문제야/또 수열 문제야.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    for(int i=0;i<N;i++)
+    {
+        printf("%d ",1+2*i);
+    }
+    return 0;
+}
