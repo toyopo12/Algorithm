61778b1d36a26b093ffb98be9a2f1871a6c4bb87
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-05-28 22:23:13 +0900

    [Silver V] Title: FIFA 월드컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4884. FIFA 월드컵/FIFA 월드컵.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while(1)
+    {
+        long long G,T,A,D;
+        scanf("%lld %lld %lld %lld",&G,&T,&A,&D);
+        if(G==-1 && T==-1 && A==-1 && D==-1)
+        {
+            break;
+        }
+        else
+        {
+            long long X=0;
+            long long Y=0;
+            int two=0;
+            long long sum=1;
+            while(sum<G*A+D)
+            {
+                sum*=2;
+                two++;
+            }
+            Y=sum-G*A-D;
+            X=G*T*(T-1)/2+sum-1;
+            printf("%lld*%lld/%lld+%lld=%lld+%lld\n",G,A,T,D,X,Y);
+        }
+    }
+    return 0;
+}
