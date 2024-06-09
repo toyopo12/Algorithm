e8cfd2c458b745edcc2b255a0e4ef37d37507a57
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-06-09 12:50:46 +0900

    [Bronze V] Title: CAPS, Time: 68 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    for(int i=0;i<s.size();i++)
+    {
+        cout << (char)(s[i]-32);
+    }
+    return 0;
+}
