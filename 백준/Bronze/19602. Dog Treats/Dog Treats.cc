76b00317bbb62d5fb200023883755eb2360d33c4
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-07-21 20:56:30 +0900

    [Bronze IV] Title: Dog Treats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a*1+b*2+c*3>=10)
+    {
+        cout << "happy";
+    }
+    else
+    {
+        cout << "sad";
+    }
+    return 0;
+}
