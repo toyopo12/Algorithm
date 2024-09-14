ae8382b0b070c80b977a0feb371e8981648cd240
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-09-15 06:14:13 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 148 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1000010];
+int lis[1000010];
+int lissize;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i];
+        if(lissize == 0){
+            lis[0] = arr[i];
+            lissize++;
+        }
+        else if(lis[lissize - 1] < arr[i]) lis[lissize++] = arr[i];
+        else{
+            int *check = lower_bound(lis, lis + lissize, arr[i]);
+            *check = arr[i];
+        }
+    }
+    cout << lissize << '\n';
+    return 0;
+}
