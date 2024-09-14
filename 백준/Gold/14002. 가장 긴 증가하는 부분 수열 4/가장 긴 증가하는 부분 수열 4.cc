13e550cf89a27c65ba41a559ab1467f082962064
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-09-15 06:12:05 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+int arr[1010];
+int lis[1010];
+int dex[1010];
+int lissize;
+stack<int> s;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i];
+        if(lissize == 0){
+            lis[0] = arr[i];
+            dex[i] = 1;
+            lissize++;
+        }
+        else if(lis[lissize - 1] < arr[i]){
+            lis[lissize++] = arr[i];
+            dex[i] = lissize;
+        }
+        else{
+            int *check = lower_bound(lis, lis + lissize, arr[i]);
+            *check = arr[i];
+            dex[i] = distance(lis, check) + 1;
+        }
+    }
+    cout << lissize << '\n';
+    for(int i = n; i > 0; i--){
+        if(dex[i] == lissize){
+            s.push(arr[i]);
+            lissize--;
+        }
+    }
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+    return 0;
+}
