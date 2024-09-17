74e21939da8c972956366d2b343a7b457a3d131e
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-09-18 05:15:21 +0900

    [Gold I] Title: 수들의 합 7, Time: 700 ms, Memory: 26240 KB -BaekjoonHub

---
+++ 백준/Gold/2268. 수들의 합 7/수들의 합 7.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#define int long long
+using namespace std;
+
+void update(vector<long long> &arr, vector<long long> &tree, int node, int start, int end, int index, long long val){
+    if(index < start || index > end) return;
+    if(start == end){
+        arr[index] = val;
+        tree[node] = val;
+        return;
+    }
+    update(arr, tree, node * 2, start, (start + end) / 2, index, val);
+    update(arr, tree, node * 2 + 1, (start + end) / 2 + 1, end, index, val);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+long long query(vector<long long> &tree, int node, int start, int end, int left, int right){
+    if(left > end || right < start) return 0;
+    if(left <= start && end <= right) return tree[node];
+    long long lsum = query(tree, node * 2, start, (start + end) / 2, left, right);
+    long long rsum = query(tree, node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+    return lsum + rsum;
+}
+
+signed main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    vector<long long> arr(n);
+    int h = (int)ceil(log2(n));
+    int tree_size = (1 << (h + 1));
+    vector<long long> tree(tree_size);
+    for(int i = 0; i < m; i++){
+        int check, a, b;
+        cin >> check >> a >> b;
+        if(check == 1) update(arr, tree, 1, 0, n - 1, a - 1, b);
+        else{
+            if(a <= b) cout << query(tree, 1, 0, n - 1, a - 1, b - 1) << '\n';
+            else cout << query(tree, 1, 0, n - 1, b - 1, a - 1) << '\n';
+        }
+    }
+    return 0;
+}
