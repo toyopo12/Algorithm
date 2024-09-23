83b815656c39b5db283ad199dac1784742ea09c9
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-09-23 20:39:20 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,46 +1,26 @@
 #include <iostream>
-#include <algorithm>
-#include <limits.h>
 
 using namespace std;
 
+int arr[10001];
+
 int main()
 {
-    int ans=0;
-    long long min=0;
-    long long max=1;
-    int K,N;
-    int arr[10010];
-    cin >> K >> N;
-    for(int i=0;i<K;i++)
-    {
-        cin >> arr[i]; 
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n, k;
+    cin >> n >> k;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    long long start = 0;
+    long long end = 0x7fffffff;
+    while(start <= end){
+        long long mid = (start + end) / 2;
+        long long big = 0;
+        for(int i = 0; i < n; i++) big += arr[i] / mid;
+        if(big < k) end = mid - 1;
+        else start = mid + 1;
     }
-    for(int i=0;i<31;i++)
-    {
-        max*=2;
-    }
-    for(int i=0;i<31;i++)
-    {
-        long long mid=(min+max)/2;
-        int check=0;
-        for(int j=0;j<K;j++)
-        {
-            check+=arr[j]/mid;
-        }
-        if(check<N)
-        {
-            max=mid;
-        }
-        else
-        {
-            if(ans<mid)
-            {
-                ans=mid;
-            }
-            min=mid;
-        }
-    }
-    cout << ans;
+    cout << end;
     return 0;
 }
