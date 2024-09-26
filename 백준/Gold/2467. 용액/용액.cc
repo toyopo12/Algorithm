0af19fabdefd37e5024f0153122e4700ac98fe3d
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-09-26 16:19:18 +0900

    [Gold V] Title: 용액, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -1,32 +1,54 @@
 #include <iostream>
+#include <algorithm>
 #include <cstdlib>
 
 using namespace std;
 
-long long arr[100010];
+int arr[100010];
+int mini = 0x7fffffff;
+int minione;
+int minitwo;
 
 int main()
 {
     ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     int n;
     cin >> n;
     for(int i = 0; i < n; i++) cin >> arr[i];
-    int start = 0, end = n - 1;
-    long long minsize = abs(arr[0] + arr[n - 1]);
-    long long minnum = arr[0];
-    long long maxnum = arr[n - 1];
-    while(start < end){
-        long long now = arr[start] + arr[end];
-        if(minsize > abs(now)){
-            minsize = abs(now);
-            minnum = arr[start];
-            maxnum = arr[end];
-        }
-        if(now < 0) start++;
-        else end--;
+    if(n == 2){
+        cout << arr[0] << " " << arr[1];
+        return 0;
     }
-    cout << minnum << " " <<  maxnum;
+    for(int i = 0; i < n - 1; i++){
+        int start = i + 1;
+        int end = n;
+        while(start < end - 1){
+            int mid = (start + end) / 2;
+            if(arr[i] + arr[mid] >= 0) end = mid;
+            else start = mid;
+        }
+        if(end == n){
+            if(abs(arr[i] + arr[start]) < abs(mini)  && start != i){
+                mini = arr[i] + arr[start];
+                minione = arr[i];
+                minitwo = arr[start];
+            }
+        }
+        else{
+            if(abs(arr[i] + arr[end]) < abs(mini)){
+                mini = arr[i] + arr[end];
+                minione = arr[i];
+                minitwo = arr[end];
+            }
+            if(abs(arr[i] + arr[start]) < abs(mini) && start != i){
+                mini = arr[i] + arr[start];
+                minione = arr[i];
+                minitwo = arr[start];
+            }
+        }
+    }
+    cout << minione << " " << minitwo;
     return 0;
 }
