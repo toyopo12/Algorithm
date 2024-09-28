41078cc6a0de1bc70abf238e635230723c1f2b5f
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-09-28 19:23:42 +0900

    [Gold III] Title: K번째 음식 찾기 2, Time: 444 ms, Memory: 4364 KB -BaekjoonHub

---
+++ 백준/Gold/23792. K번째 음식 찾기 2/K번째 음식 찾기 2.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+long long arr[100010];
+long long brr[100010];
+long long crr[100010];
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, q;
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    for(int i = 0; i < n; i++) cin >> brr[i];
+    for(int i = 0; i < n; i++) cin >> crr[i];
+    cin >> q;
+    int x, y, z, k;
+    while(q--){
+        cin >> x >> y >> z >> k;
+        long long start = 0;
+        long long end = 3000000000;
+        while(start < end - 1){
+            long long mid = (start + end) / 2;
+            int acount = upper_bound(arr, arr + x, mid) - arr;
+            int bcount = upper_bound(brr, brr + y, mid) - brr;
+            int ccount = upper_bound(crr, crr + z, mid) - crr;
+            if(acount + bcount + ccount > k) end = mid;
+            else if(acount + bcount + ccount < k) start = mid;
+            else{
+                if(arr[acount - 1] < brr[bcount - 1]){
+                    if(brr[bcount - 1] < crr[ccount - 1]) cout << "3 " << ccount;
+                    else cout << "2 " << bcount;
+                }
+                else{
+                    if(arr[acount - 1] < crr[ccount - 1]) cout << "3 " << ccount;
+                    else cout << "1 " << acount;
+                }
+                cout << '\n';
+                break;
+            }
+        }
+    }
+    return 0;
+}
