8c5b38ddc9448a83dde0e842868ace8e71204d42
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-12-16 01:59:08 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int N; cin >> N;
+    for(int k = 0; k < 2; k++) {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) cout << "@";
+            for(int j = 0; j < 3 * N; j++) cout << " ";
+            for(int j = 0; j < N; j++) cout << "@";
+            cout << '\n';
+        }
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < 5 * N; j++) cout << "@";
+            cout << '\n';
+        }
+    }
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) cout << "@";
+        for(int j = 0; j < 3 * N; j++) cout << " ";
+        for(int j = 0; j < N; j++) cout << "@";
+        cout << '\n';
+    }
+    return 0;
+}
