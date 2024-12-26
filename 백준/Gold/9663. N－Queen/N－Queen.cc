76b09f92ec048e35a0c2a28fc0c7df3a05a4cf5f
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2024-12-26 15:18:57 +0900

    [Gold IV] Title: N-Queen, Time: 4624 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -7,30 +7,19 @@ int visited[16];
 int sum;
 int n;
 
-void good(int x)
-{
-    if(x==n)
-    {
-        sum++;
-    }
-    else
-    {
-        for(int i=0;i<n;i++)
-        {
-            bool check=true;
-            visited[x]=i;
-            for(int j=0;j<x;j++)
-            {
-                if(visited[j]==i || visited[j]==i+x-j || visited[j]==i-x+j)
-                {
-                    check=false;
+void good(int x) {
+    if(x == n) sum++;
+    else {
+        for(int i = 0; i < n; i++) {
+            bool check = true;
+            visited[x] = i;
+            for(int j = 0; j < x; j++) {
+                if(visited[j] == i || visited[j] == i + x - j || visited[j] == i - x + j) {
+                    check = false;
                     break;
                 }
             }
-            if(check)
-            {
-                good(x+1);
-            }
+            if(check) good(x + 1);
         }
     }
 }
