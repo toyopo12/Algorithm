d45a094a0e02884bf0b7e6bb8dc5bfcdd2db31ab
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2025-04-09 12:49:27 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,11 +1,2 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    int a, b;
-    cin >> a >> b;
-    cout << a + b;
-    return 0;
-}
+#include<iostream>
+main(){int a,b;std::cin>>a>>b;std::cout<<a+b;}
