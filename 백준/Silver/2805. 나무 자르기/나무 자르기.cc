89c395aa30cabbc42bdf806b643f957b96ed05b0
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2025-06-09 10:43:48 +0900

    [Silver II] Title: 나무 자르기, Time: 144 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -2,29 +2,25 @@
 
 using namespace std;
 
-int arr[1000001];
+int tree[1000001];
 
 int main()
 {
-    ios_base :: sync_with_stdio(false);
+    ios_base ::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    int n, m;
+    long long n, m;
     cin >> n >> m;
-    for(int i = 0; i < n; i++) cin >> arr[i];
+    for(int i = 0; i < n; i++) cin >> tree[i];
     int start = 0;
-    int end = 1000000000;
-    while(start <= end){
+    int end = 1000000001;
+    while(start < end - 1){
+        long long good = 0;
         int mid = (start + end) / 2;
-        long long big = 0;
-        for(int i = n - 1; i >= 0; i--) if(arr[i] > mid) big += arr[i] - mid;
-        if(big < m) end = mid - 1;
-        else if(big > m) start = mid + 1;
-        else{
-            cout << mid;
-            return 0;
-        }
+        for(int i = 0; i < n; i++) if(tree[i] > mid) good += tree[i] - mid;
+        if(good < m) end = mid;
+        else start = mid;
     }
-    cout << end;
+    cout << start;
     return 0;
 }
