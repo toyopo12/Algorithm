d577fd34ff15a170b6803171d6eca19a68976769
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2025-07-22 13:05:38 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    long long x; cin >> x;
+    cout << x * x * x;
+
+    return 0;
+}
