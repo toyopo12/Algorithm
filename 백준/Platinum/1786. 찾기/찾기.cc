812f603a3da846106521cdaefbf938d837aba010
toyopo12 <159609170+toyopo12@users.noreply.github.com>
2025-08-04 23:25:43 +0900

    [Platinum V] Title: 찾기, Time: 52 ms, Memory: 9432 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.cc
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -3,14 +3,10 @@
 #include <string>
 using namespace std;
 
-int counting;
-vector<int> point;
-
 vector<int> failure(string& s) {
     vector<int> f(s.size());
-    int j = 0;
-    for (int i = 1; i < s.size(); i++) {
-        while (j > 0 && s[i] != s[j]) j = f[j - 1];
+    for (int i = 1, j = 0; i < s.size(); i++) {
+        while (j && s[i] != s[j]) j = f[j - 1];
         if (s[i] == s[j]) f[i] = ++j;
     }
     return f;
@@ -20,22 +16,19 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    vector<int> point;
     string T, P;
     getline(cin, T);
     getline(cin, P);
     vector<int>f = failure(P);
-    int j = 0;
-    for (int i = 0; i < T.size(); i++) {
-        while (j > 0 && T[i] != P[j]) j = f[j - 1];
+    for (int i = 0, j = 0; i < T.size(); i++) {
+        while (j && T[i] != P[j]) j = f[j - 1];
         if (T[i] == P[j]) j++;
         if (j == P.size()) {
-            counting++;
-            point.push_back(i - P.size() + 2);
+            point.push_back(i - j + 2);
         }
     }
-    cout << counting << '\n';
-    for (int i = 0; i < point.size(); i++) {
-        cout << point[i] << " ";
-    }
+    cout << point.size() << '\n';
+    for (auto&i : point) cout << i << " ";
     return 0;
 }
